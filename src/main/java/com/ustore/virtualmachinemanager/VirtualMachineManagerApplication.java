3d99550ed6963c585e9070fee1f3040537cb4f91
Davi Queiroz <daviqueiroz92@gmail.com>
2019-11-29 08:03:04 -0300

    Bootstrap project

---
+++ src/main/java/com/ustore/virtualmachinemanager/VirtualMachineManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ustore.virtualmachinemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualMachineManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualMachineManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ustore/virtualmachinemanager/VirtualMachineManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ustore.virtualmachinemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualMachineManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
